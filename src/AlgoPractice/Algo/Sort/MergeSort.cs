02474b3f9488a904b4f50a254d3c38d94c2aa711
paytonli2013 <payton.com@163.com>
2014-11-30 22:46:05 +0800

    Merge sort

---
+++ src/AlgoPractice/Algo/Sort/MergeSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algo.Sort
+{
+    public class MergeSort : IAlgo<IInput<int[]>, int[]>
+    {
+        const int RECURESION_CUTOFF = 32;
+        #region IAlgo<IInput<string[]>,string[]> Members
+
+        public int[] Compute(IInput<int[]> input)
+        {
+            return Sort(input.Input);
+        }
+        private int[] Sort(int[] input)
+        {
+            var array = input;
+            int length = array.Length;
+            if (length <= RECURESION_CUTOFF)
+            {
+                Array.Sort(array);
+                return array;
+            }
+
+            int n = (length + 1) / 2;
+            int[] left = new int[n];
+            int[] right = new int[length - n];
+            array.CopyTo(left,0);
+
+            Array.Copy(array, left, n);
+            Array.Copy(array, n, right, 0, length - n);
+
+            return Combine(Sort(left), Sort(right));
+        }
+
+        private int[] Combine(int[] array1, int[] array2)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
