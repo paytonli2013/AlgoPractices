eb3c63133f8d0a54e64dc4c48b2960457187bb64
paytonli2013 <payton.com@163.com>
2014-11-30 15:26:11 +0800

    initial commit

---
+++ src/AlgoPractice/Algo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algo
+{
+    public interface IAlgo<TInput,TOutput>
+    {
+        TOutput Compute(TInput input);
+    }
+}
