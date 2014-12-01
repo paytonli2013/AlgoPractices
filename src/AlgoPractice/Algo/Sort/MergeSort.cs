12eea2f29562df4f18e7de2b1c90ac4c97524e3d
paytonli2013 <payton.com@163.com>
2014-12-01 23:31:18 +0800

    complete merge sort

--- src/AlgoPractice/Algo/Sort/MergeSort.cs
+++ src/AlgoPractice/Algo/Sort/MergeSort.cs
@@ -7,7 +7,7 @@ namespace Algo.Sort
 {
     public class MergeSort : IAlgo<IInput<int[]>, int[]>
     {
-        const int RECURESION_CUTOFF = 32;
+        const int RECURESION_CUTOFF = 8;
         #region IAlgo<IInput<string[]>,string[]> Members
 
         public int[] Compute(IInput<int[]> input)
@@ -27,7 +27,6 @@ namespace Algo.Sort
             int n = (length + 1) / 2;
             int[] left = new int[n];
             int[] right = new int[length - n];
-            array.CopyTo(left,0);
 
             Array.Copy(array, left, n);
             Array.Copy(array, n, right, 0, length - n);
@@ -37,7 +36,37 @@ namespace Algo.Sort
 
         private int[] Combine(int[] array1, int[] array2)
         {
-            throw new NotImplementedException();
+            int length = array1.Length + array2.Length;
+            int[] result = new int[length];
+
+            int i = 0, j = 0, k = 0;
+
+            while (k < length)
+            {
+                if (i == array1.Length)
+                {
+                    result[k] = array2[j];
+                    j++;
+                }
+                else if (j == array2.Length)
+                {
+                    result[k] = array1[i];
+                    i++;
+                }
+                else if ((array1[i] <= array2[j]))
+                {
+                    result[k] = array1[i];
+                    i++;
+                }
+                else
+                {
+                    result[k] = array2[j];
+                    j++;
+                }
+                k++;
+            }
+
+            return result;
         }
 
         #endregion
